08c62ff43261c40cc9f375ac93c6b8f8c8300adf
saipratap99 <122005067@sastra.ac.in>
2021-12-22 15:11:37 +0530

    Ploted by varrying voltage from 200 to 300V with 25V step

--- WPT_SCRIPT_OPTIMAL_voltage.m
+++ WPT_SCRIPT_OPTIMAL_voltage.m
@@ -4,24 +4,26 @@ clc;
 L1 = 375.48e-6;
 L2 = 375.48e-6;
 M = 344.58e-06;
-freq = [100:100:1000 2000:1000:10000];
-V = 230;
+voltages = [200:25:300];
+f = 10000;
+Inverter_period = 1/f;
+Inverter_phase_delay = Inverter_period/2;
+C1 = 1/(4*pi*pi*f*f*L1);
+C2 = 1/(4*pi*pi*f*f*L2);    
+
 effi = [];
-for f = freq
-    Inverter_period = 1/f;
-    Inverter_phase_delay = Inverter_period/2;
-    C1 = 1/(4*pi*pi*f*f*L1);
-    C2 = 1/(4*pi*pi*f*f*L2);
+for v = voltages
+    V = v;
     out = sim('WPT_19b');
     pin_index = size(out.Pin.Time,1);
     Pin = out.Pin.Data(pin_index,2);
     pout_index = size(out.Pout.Time,1);
     Pout = out.Pout.Data(pout_index,1);
     effi(length(effi) + 1) = Pout/Pin*100;
-    fprintf('f: %8d, C: %2.8f, Pin: %4.2f, Pout: %4.2f, n: %4.2f\n',f,C1,Pin,Pout,effi(length(effi)));
+    fprintf('V: %5d, Pin: %4.2f, Pout: %4.2f, n: %4.2f\n',V,Pin,Pout,effi(length(effi)));
 end
 
 
-plot(freq,effi,'b-o')
+plot(voltages,effi,'b-o')
 xlabel('frequency');
 ylabel('efficieny');
